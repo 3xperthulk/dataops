2c308226e5f25df9aad7c99969ae0cf283fa43c6
3xperthulk <41061831+3xperthulk@users.noreply.github.com>
2024-10-20 17:20:02 +0530

    with service principal

--- terraform/modules/storage-acc-container/variables.tf
+++ terraform/modules/storage-acc-container/variables.tf
@@ -8,27 +8,7 @@ variable "location" {
   type        = string
 }
 
-variable "account_tier" {
-  description = "Defines the Tier to use for this storage account. Valid options are Standard and Premium."
-  type        = string
-}
-
-variable "account_replication_type" {
-  description = "Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS, and ZRS."
-  type        = string
-}
-
 variable "environment" {
   description = "Tag to specify the environment (e.g., dev, staging, production)."
   type        = string
-}
-
-variable "container_access_type" {
-  description = "Container access type"
-  type = string
-}
-
-variable "public_access_policy" {
-  description = "policy access type"
-  type = string
 }
